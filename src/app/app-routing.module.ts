0a40dc0e0fbcca9bf38c0b7a1cb33efe46c4d634
Sasikala-Tammireddi <sravanisasi543@gmail.com>
2020-01-02 14:50:51 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,6 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  },
-  {
     path: 'contact-list',
     loadChildren: () => import('./pages/contact-list/contact-list.module').then( m => m.ContactListPageModule)
   },
